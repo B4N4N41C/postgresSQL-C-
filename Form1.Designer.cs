920ac63c62d870a8d059a774293695ecbcc02f00
B4N4N41C <mochalin.nikolay2017@yandex.ru>
2023-10-27 23:57:31 +0300

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace laba3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvData = new System.Windows.Forms.DataGridView();
+            this.txtFirstname = new System.Windows.Forms.TextBox();
+            this.txtLastname = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMidname = new System.Windows.Forms.TextBox();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtCurse = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMajor = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvData
+            // 
+            this.dgvData.AllowUserToAddRows = false;
+            this.dgvData.AllowUserToDeleteRows = false;
+            this.dgvData.AllowUserToOrderColumns = true;
+            this.dgvData.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvData.Location = new System.Drawing.Point(12, 15);
+            this.dgvData.MultiSelect = false;
+            this.dgvData.Name = "dgvData";
+            this.dgvData.ReadOnly = true;
+            this.dgvData.RowHeadersWidth = 82;
+            this.dgvData.RowTemplate.Height = 33;
+            this.dgvData.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvData.Size = new System.Drawing.Size(991, 496);
+            this.dgvData.TabIndex = 0;
+            this.dgvData.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvData_CellClick);
+            // 
+            // txtFirstname
+            // 
+            this.txtFirstname.Enabled = false;
+            this.txtFirstname.Location = new System.Drawing.Point(1446, 18);
+            this.txtFirstname.Multiline = true;
+            this.txtFirstname.Name = "txtFirstname";
+            this.txtFirstname.Size = new System.Drawing.Size(332, 49);
+            this.txtFirstname.TabIndex = 1;
+            // 
+            // txtLastname
+            // 
+            this.txtLastname.Enabled = false;
+            this.txtLastname.Location = new System.Drawing.Point(1446, 125);
+            this.txtLastname.Multiline = true;
+            this.txtLastname.Name = "txtLastname";
+            this.txtLastname.Size = new System.Drawing.Size(332, 46);
+            this.txtLastname.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1336, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Отчество";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1337, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Фамилия";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1387, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Имя";
+            // 
+            // txtMidname
+            // 
+            this.txtMidname.Enabled = false;
+            this.txtMidname.Location = new System.Drawing.Point(1446, 73);
+            this.txtMidname.Multiline = true;
+            this.txtMidname.Name = "txtMidname";
+            this.txtMidname.Size = new System.Drawing.Size(332, 46);
+            this.txtMidname.TabIndex = 1;
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(1009, 18);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(267, 46);
+            this.btnInsert.TabIndex = 3;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(1009, 70);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(267, 46);
+            this.btnUpdate.TabIndex = 4;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(1009, 122);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(267, 46);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(1009, 174);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(267, 46);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // txtCurse
+            // 
+            this.txtCurse.Enabled = false;
+            this.txtCurse.Location = new System.Drawing.Point(1446, 177);
+            this.txtCurse.Multiline = true;
+            this.txtCurse.Name = "txtCurse";
+            this.txtCurse.Size = new System.Drawing.Size(332, 46);
+            this.txtCurse.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1381, 180);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Курс";
+            // 
+            // txtMajor
+            // 
+            this.txtMajor.Enabled = false;
+            this.txtMajor.Location = new System.Drawing.Point(1446, 229);
+            this.txtMajor.Multiline = true;
+            this.txtMajor.Name = "txtMajor";
+            this.txtMajor.Size = new System.Drawing.Size(332, 46);
+            this.txtMajor.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1294, 232);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(146, 25);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Направление";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1790, 533);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtMajor);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtCurse);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMidname);
+            this.Controls.Add(this.txtLastname);
+            this.Controls.Add(this.txtFirstname);
+            this.Controls.Add(this.dgvData);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvData;
+        private System.Windows.Forms.TextBox txtFirstname;
+        private System.Windows.Forms.TextBox txtLastname;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMidname;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtCurse;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtMajor;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,180 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace laba3
+{
+    public partial class Form1 : Form
+    {
+        private string connstring = String.Format("Server=localhost;Port=5432;User Id=postgres;Password=toor;Database=postgres;");
+        private NpgsqlConnection conn;
+        private string sql;
+        private NpgsqlCommand cmd;
+        private DataTable dt;
+        private int rowIndex = -1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conn = new NpgsqlConnection(connstring);
+            Select();
+        }
+
+        private void Select()
+        {
+            try
+            {
+                conn.Open();
+                sql = @"SELECT * FROM public.""Student"";";
+                cmd = new NpgsqlCommand(sql, conn);
+                dt = new DataTable();
+                dt.Load(cmd.ExecuteReader());
+                conn.Close();
+                dgvData.DataSource = null;
+                dgvData.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                conn.Close();
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void dgvData_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(e.RowIndex >= 0)
+            {
+                rowIndex = e.RowIndex;
+                txtFirstname.Text = dgvData.Rows[e.RowIndex].Cells["st_firstname"].Value.ToString();
+                txtMidname.Text = dgvData.Rows[e.RowIndex].Cells["st_midname"].Value.ToString();
+                txtLastname.Text = dgvData.Rows[e.RowIndex].Cells["st_lastname"].Value.ToString();
+                txtCurse.Text = dgvData.Rows[e.RowIndex].Cells["st_curse"].Value.ToString();
+                txtMajor.Text = dgvData.Rows[e.RowIndex].Cells["st_major"].Value.ToString();
+            }
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            rowIndex = -1;
+            txtFirstname.Enabled = txtMidname.Enabled = txtLastname.Enabled = txtCurse.Enabled = txtMajor.Enabled = true;
+            txtFirstname.Text = txtMidname.Text = txtLastname.Text = txtCurse.Text = txtMajor.Text = null;
+            txtFirstname.Select();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if(rowIndex < 0)
+            {
+                MessageBox.Show("Пожалуйста выберите студента для обновления");
+                return;
+            }
+            txtFirstname.Enabled = txtMidname.Enabled = txtLastname.Enabled = txtCurse.Enabled = txtMajor.Enabled = true;
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int result = 0;
+            if (rowIndex < 0)
+            {
+                MessageBox.Show("Пожалуйста выберите студента для удаления");
+                return;
+            }
+            try
+            {
+                conn.Open();
+                sql = $@"DELETE FROM public.""Student"" WHERE st_id = {int.Parse(dgvData.Rows[rowIndex].Cells["st_id"].Value.ToString())};";
+                cmd = new  NpgsqlCommand(sql, conn);
+                result = cmd.ExecuteNonQuery();
+                conn.Close();
+                if (result == 1)
+                {
+                    MessageBox.Show("Удаление выполнено");
+                    rowIndex = -1;
+                    Select();
+                }
+            }
+            catch(Exception ex)
+            {
+                conn.Close();
+                MessageBox.Show("Ошибка удаления. Error"+ex.Message);
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            int result = 0;  
+            if (rowIndex < 0)//insert
+            {
+                try
+                {
+                    conn.Open();
+                    sql = $@"INSERT INTO public.""Student""(st_firstname, st_midname, st_lastname, st_curse, st_major) VALUES ('{txtFirstname.Text}', '{txtMidname.Text}', '{txtLastname.Text}', '{txtCurse.Text}', '{txtMajor}');";
+                    
+                    cmd = new NpgsqlCommand(sql, conn);
+                    result = (int)cmd.ExecuteNonQuery();
+                    conn.Close();
+                    if(result == 1)
+                    {
+                        MessageBox.Show("Новый студент добавлен");
+                        Select();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Ошибка добавления");
+                    }
+                    
+                }
+                catch (Exception ex)
+                {
+                    conn.Close();
+                    MessageBox.Show("Ошибка добавления. Error:" + ex.Message);
+                }
+            }
+            else//update
+            {
+                try
+                {
+                    conn.Open();
+                    //SELECT * FROM public."Student" WHERE st_firstname = 'dsd' AND st_midname = 'dsd' AND st_lastname = 'dsd';
+                    //UPDATE public."Student" SET st_firstname =?, st_midname =?, st_lastname =? WHERE<condition>;
+                    sql = $@"UPDATE public.""Student"" SET st_firstname = '{txtFirstname.Text}', st_midname = '{txtMidname.Text}', st_lastname = '{txtLastname.Text}', st_curse = '{txtCurse.Text}', st_major = '{txtMajor.Text}'  WHERE st_id = {int.Parse(dgvData.Rows[rowIndex].Cells["st_id"].Value.ToString())}";
+                    cmd = new NpgsqlCommand(sql, conn);
+                    result = (int)cmd.ExecuteNonQuery();
+                    conn.Close();
+                    if(result == 1)
+                    {
+                        MessageBox.Show("Update successfully");
+                        Select();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Update fail");
+                    }
+                    Select();
+                }
+                catch (Exception ex)
+                {
+                    conn.Close();
+                    MessageBox.Show("Updated fail. Error:" + ex.Message);
+                }
+            }
+            result = 0;
+            txtFirstname.Text = txtMidname.Text = txtLastname.Text = txtCurse.Text = txtMajor.Text = null;
+            txtFirstname.Enabled = txtMidname.Enabled = txtLastname.Enabled = txtCurse.Enabled = txtMajor.Enabled = false;
+        }
+    }
+}
